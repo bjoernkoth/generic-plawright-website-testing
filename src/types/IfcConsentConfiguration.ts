fa1f98207fbd61d2a399573198760ba9ba5a9d99
Bjoern Koth <bjoern.koth@valtech.com>
2024-08-17 10:04:15 +0200

    Renamed "options" to "refinements" to narrow down the consent accept locator

--- src/types/IfcConsentConfiguration.ts
+++ src/types/IfcConsentConfiguration.ts
@@ -6,5 +6,5 @@ export interface IfcConsentConfiguration {
     locator?: string; // locator to find the consent element
     role?: any; // role to find the consent element
     label?: string; // label to find the consent element
-    options?: Object; // options to find the consent element
+    refinements?: Object; // additional refinements to find the consent element after the initial locator is found
 }
